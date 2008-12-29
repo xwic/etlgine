ac59b310ab6c98e431c2b145b3c014b464b8afca
lippisch <Florian.Lippisch@netapp.com>
2008-12-29 18:12:07 +0000

    Added ETLgine Administration UI

---
+++ de.xwic.etlgine/server/xcube/ETLpoolInit.groovy
@@ -0,0 +1,16 @@
+/**
+ * Ensure that the specified dimensions exist.
+ */
+util.ensureDimension "Name"
+util.ensureDimension "Area"
+util.ensureElement "Area", "EMEA"
+util.ensureElement "Area", "EMEA/HQ"
+util.ensureElement "Area", "EMEA/Germany"
+util.ensureElement "Area", "EMEA/UK"
+util.ensureElement "Area", "EMEA/France"
+
+util.ensureMeasure "Bookings"
+util.ensureMeasure "Plan"
+
+util.ensureCube "Test", ["Name", "Area"], ["Bookings"]
+

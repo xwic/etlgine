75f522e8d779800a2ed6dd1a134cb719b29e943b
lippisch <Florian.Lippisch@netapp.com>
2009-01-22 07:38:55 +0000

    fixed run.bat

--- de.xwic.etlgine/server/run.bat
+++ de.xwic.etlgine/server/run.bat
@@ -2,7 +2,6 @@
 CALL setcp.bat
 ECHO .
 ECHO .
-if "%1%" == "" GOTO err
 JAVA -server -cp %ETLCP% -Xmx768m de.xwic.etlgine.server.Launch %1 %2 %3 %4 %5 %6 %7 %8 %9
 goto end
 :err
@@ -11,3 +10,4 @@ ECHO .
 ECHO You must run the run_XXXX batch files.
 
 :end
+pause

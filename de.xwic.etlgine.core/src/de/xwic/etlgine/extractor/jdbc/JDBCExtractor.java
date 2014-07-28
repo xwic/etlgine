f708ea0b5ad10b3978d1833d9a89b197de373784
Eugen <eugen@netapp.com>
2014-07-28 12:41:18 +0300

    if oracle and specific scale - special handling

--- de.xwic.etlgine.core/src/de/xwic/etlgine/extractor/jdbc/JDBCExtractor.java
+++ de.xwic.etlgine.core/src/de/xwic/etlgine/extractor/jdbc/JDBCExtractor.java
@@ -251,7 +251,7 @@ public class JDBCExtractor extends AbstractExtractor {
 					dt = DataType.STRING;
 					break;
 				case Types.NUMERIC: // Oracle NUMBER is handled here
-					if (scale > 0) {
+					if (scale > 0 || (scale == -127 && currSource.getDriverName().contains("OracleDriver"))) {
 						dt = DataType.DOUBLE;
 					} else if (lengthHint > 10) {
 						dt = DataType.LONG;

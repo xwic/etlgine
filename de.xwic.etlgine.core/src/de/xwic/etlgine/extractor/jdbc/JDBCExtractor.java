cffd7f27fd8f346a9e394c4a9ded85203b3f4a5c
Eugen <eugen@netapp.com>
2014-07-30 13:47:59 +0300

    undo scale -127

--- de.xwic.etlgine.core/src/de/xwic/etlgine/extractor/jdbc/JDBCExtractor.java
+++ de.xwic.etlgine.core/src/de/xwic/etlgine/extractor/jdbc/JDBCExtractor.java
@@ -251,7 +251,8 @@ public class JDBCExtractor extends AbstractExtractor {
 					dt = DataType.STRING;
 					break;
 				case Types.NUMERIC: // Oracle NUMBER is handled here
-					if (scale > 0 || (scale == -127 && connection.getMetaData().getURL().contains("oracle"))) {
+					//if (scale > 0 || (scale == -127 && connection.getMetaData().getURL().contains("oracle"))) {
+					if (scale > 0) {
 						dt = DataType.DOUBLE;
 					} else if (lengthHint > 10) {
 						dt = DataType.LONG;

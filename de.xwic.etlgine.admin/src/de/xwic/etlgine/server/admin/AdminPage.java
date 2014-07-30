91c0572168073828b0c37327402d5c3f38f9c6ba
Eugen <eugen@netapp.com>
2014-07-30 08:06:26 +0300

    add version info

--- de.xwic.etlgine.admin/src/de/xwic/etlgine/server/admin/AdminPage.java
+++ de.xwic.etlgine.admin/src/de/xwic/etlgine/server/admin/AdminPage.java
@@ -3,16 +3,14 @@
  */
 package de.xwic.etlgine.server.admin;
 
-import java.util.List;
+import org.apache.commons.lang.StringUtils;
 
 import de.jwic.base.IControlContainer;
 import de.jwic.base.Page;
 import de.jwic.controls.Button;
+import de.jwic.controls.Label;
 import de.jwic.events.SelectionEvent;
 import de.jwic.events.SelectionListener;
-import de.xwic.etlgine.impl.Context;
-import de.xwic.etlgine.publish.CubePublishDestination;
-import de.xwic.etlgine.publish.CubePublisherManager;
 import de.xwic.etlgine.server.ETLgineServer;
 import de.xwic.etlgine.server.ServerContext;
 
@@ -55,6 +53,8 @@ public class AdminPage extends Page {
 		});
 
 		new OnOffControl(this, "on_off");
+		
+		createVersionInfoControl(this);
 	}
 	
 	/**
@@ -68,4 +68,34 @@ public class AdminPage extends Page {
 		return ETLgineServer.getInstance().getServerContext().getProperty(ServerContext.PROPERTY_SERVER_INSTANCEID, ServerContext.PROPERTY_SERVER_INSTANCEID_DEFAULT);
 	}
 	
+	protected void createVersionInfoControl(IControlContainer container) {
+		// set the version label
+		Label verInfo = new Label(container, "lblVersionInfo");
+        log.info( "  Implementation Title:" + this.getClass().getPackage().getImplementationTitle() );
+        log.info( " Implementation Vendor:" + this.getClass().getPackage().getImplementationVendor() );
+        log.info( "Implementation Version:" + this.getClass().getPackage().getImplementationVersion() );
+        log.info( "    Specification Tile:" + this.getClass().getPackage().getSpecificationTitle() );
+        log.info( "  Specification Vendor:" + this.getClass().getPackage().getSpecificationVendor() );
+        log.info( " Specification Version:" + this.getClass().getPackage().getSpecificationVersion() );
+
+		String implementationVersion =this.getClass().getPackage().getSpecificationVersion();
+		log.info("Got data - " + implementationVersion);
+		if (!StringUtils.isEmpty(implementationVersion)) {
+			log.info("Not EMPTY!");
+			if(implementationVersion.contains("SNAPSHOT")) {
+				log.info("SNAPSHOT!");
+				implementationVersion = implementationVersion +  "(#" + this.getClass().getPackage().getImplementationVersion()+")";
+			} else {
+				log.info("NO SNAPSHOT!");
+				implementationVersion =  implementationVersion + "." + this.getClass().getPackage().getImplementationVersion();
+			}
+		} else {
+			log.info("EMPTY!");
+			implementationVersion = "5.0.0.0-SNAPSHOT";
+		}
+		verInfo.setText(getServerInstance() 
+				+ " | v"
+				+ implementationVersion );
+	}
+
 }

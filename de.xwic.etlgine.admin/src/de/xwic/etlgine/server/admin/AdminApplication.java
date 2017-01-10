f74f5b6541854f97e951e03d2a0c4a033cc6fd93
Eugen <eugen@netapp.com>
2017-01-10 13:32:42 +0200

    play around

--- de.xwic.etlgine.admin/src/de/xwic/etlgine/server/admin/AdminApplication.java
+++ de.xwic.etlgine.admin/src/de/xwic/etlgine/server/admin/AdminApplication.java
@@ -3,15 +3,16 @@
  */
 package de.xwic.etlgine.server.admin;
 
-import de.jwic.base.Application;
 import de.jwic.base.Control;
 import de.jwic.base.IControlContainer;
+import de.xwic.appkit.webbase.toolkit.app.ExtendedApplication;
+import de.xwic.appkit.webbase.toolkit.app.Site;
 
 /**
  * @author Developer
  *
  */
-public class AdminApplication extends Application {
+public class AdminApplication extends ExtendedApplication {
 
 	/* (non-Javadoc)
 	 * @see de.jwic.base.Application#createRootControl(de.jwic.base.IControlContainer)
@@ -21,6 +22,24 @@ public class AdminApplication extends Application {
 		container.getSessionContext().setExitURL("byebye.html");
 		AdminPage page = new AdminPage(container);
 		return page;
+	}
+
+	@Override
+	public String getPageTitle() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getHelpURL() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected void loadApp(Site site) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
